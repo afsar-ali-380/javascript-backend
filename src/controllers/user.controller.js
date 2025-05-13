cf606431ca16cdc7c4f71108beb69acd1fd77e04
afsar-ali-380 <afsar380535@gmail.com>
2025-05-13 15:51:41 +0530

    created register controller with file uploading

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,58 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { registerSchema } from "../validations/user.validation.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "ok" });
-});
+  const parsed = registerSchema.safeParse(req.body);
 
-// export { registerUser };
+  if (!parsed.success) {
+    throw new ApiError(
+      400,
+      "Validation Failed",
+      parsed.error.flatten().fieldErrors
+    );
+  }
+
+  const { username, email, fullName, password } = parsed.data;
+
+  const preUser = await User.findOne({ $or: [{ username }, { email }] });
+
+  if (preUser) {
+    throw new ApiError(409, "User already registered with username or email");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const newUser = await User.create({
+    username: username.toLowerCase(),
+    email,
+    fullName,
+    password,
+    avatar: avatar.url,
+  });
+
+  const createdUser = User.findById(newUser._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong! while creating user");
+  }
+
+  const response = new ApiResponse(200);
+
+  return res.status(response.statusCode).json(response);
+});
